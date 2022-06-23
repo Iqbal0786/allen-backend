9a0e2864525684f682a8b20ec9bef6a1e2ff36a8
Mohammad Iqbal <iqbal240456@gmail.com>
2022-06-23 15:15:44 +0530

    some changes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-const express= require("express")
-const cors= require("cors")
-const connect= require("./config/db")
+const express= require("express");
+const connect =require("./config/db")
 const { register, login } = require("./controllers/auth.controller");
-const app= express()
-app.use(express.json())
-app.use(cors())
+const userController= require("./controllers/user.controller")
+const cors=require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/users",userController)
+
 app.post("/register", register)
 app.post("/login", login)
 
-app.listen(process.env.PORT || 8888 , async ()=>{
-        try {
-           await connect()  
-           console.log("listening port number 8888")
-          
-        } catch (error) {
-            console.log(error.message)
-        }
+
+app.get("",async(req,res)=>{
+  try {
+    return res.send("Server is live now")
+  } catch (error) {
+    return res.send(error.message)
+  }
+})
+
+const port=process.env.PORT||8888;
+app.listen(port,async()=>{
+    try {
+         await connect();
+         console.log("Listening the port number 8888");
+    } catch (error) {
+        console.log(error.message)
+    }
 })
